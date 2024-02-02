c7082123d420f5af90b5920f658b0f7e0090b3f8
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-02-02 15:36:31 +0700

    add api for payment

--- checkout/CheckoutClient.tsx
+++ checkout/CheckoutClient.tsx
@@ -3,13 +3,18 @@
 import { useCart } from "@/hooks/useCart";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const CheckoutClient = () => {
   const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
 
-  const router = useRouter()
+  const router = useRouter();
+
+  console.log("paymentIntent", paymentIntent);
+  console.log("clientSecret", clientSecret);
 
   useEffect(() => {
     if (cartProducts) {
@@ -23,13 +28,23 @@ const CheckoutClient = () => {
           items: cartProducts,
           payment_intent_id: paymentIntent,
         }),
-      }).then((res) => {
-        setLoading(false)
-        if(res.status === 401){
-            return router.push('/login')
-        }
-        return res.json()
       })
+        .then((res) => {
+          setLoading(false);
+          if (res.status === 401) {
+            return router.push("/login");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setClientSecret(data.paymentIntent.client_secret);
+          handleSetPaymentIntent(data.paymentIntent.id);
+        })
+        .catch((error) => {
+          setError(true);
+          console.log("Error", error);
+          toast.error("Something went wrong!");
+        });
     }
   }, [cartProducts, paymentIntent]);
   return <div>Checkout</div>;
